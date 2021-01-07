423d71d9ff4493d46d5f61c5110ee7d96ac0cce1
vishal-git21 <v1sh4lrr@gmail.com>
2021-01-07 22:20:48 +0530

    comment

--- networking/blog/urls.py
+++ networking/blog/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib import admin 
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,UserPostListView
 from . import views
 from .views import *
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
